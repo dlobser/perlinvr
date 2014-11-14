10c98eac013ec4272a765621523d8382b27683fa
Karen <karenpenglabs@gmail.com>
2014-11-14 17:55:31 -0500

    img folder to hold sprites

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("views", __dirname);
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 
-app.use(express.static(__dirname + '/script'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
 
