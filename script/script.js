519bbc1db6a54e13db5fc189153b7428fc546791
David Lobser <david@172-16-244-73.dynapool.nyu.edu>
2014-11-14 18:48:27 -0500

    added balls

--- script/script.js
+++ script/script.js
@@ -9,8 +9,8 @@
 				MOVE_SPEED = 1,
 				SLOW_SPEED = MOVE_SPEED / 2,
 
-				socket = io.connect('https://agile-brook-2507.herokuapp.com/'),
-								// socket = io.connect('http://localhost'),
+				// socket = io.connect('https://agile-brook-2507.herokuapp.com/'),
+								socket = io.connect('http://localhost'),
 
 				camera,
 				head,
@@ -129,6 +129,8 @@
 
 		function animate() {
 
+        removeLines();
+
 				updatePosition();
 				vrControls.update();
 				// console.log(otherInfo.pos);
@@ -158,17 +160,17 @@
 							}
 							tempCurves.push(tCurve);
 						}
-						for(var i = 0 ; i < curves.length ; i++){
-							tempCurves.push(curves[i]);
-						}
+						// for(var i = 0 ; i < curves.length ; i++){
+						// 	tempCurves.push(curves[i]);
+						// }
 						tempCurves.redraw = true;
 						drawLine(tempCurves);
 						// drawLine(curves);
 						// console.log(tempCurves);
 						// console.log(curves);
 					}
-					else
-						drawLine(curves);
+					// else
+					// 	drawLine(curves);
 				}
 				else{
 					otherHead.quaternion.copy(camera.quaternion);
@@ -180,8 +182,10 @@
 					otherHead.position.z = vec.z;
 					curves.redraw = true;
 					//console.log(curves);
-					drawLine(curves);
+					
 				}
+        curves.redraw = true;
+        drawLine(curves);
 
 
 				socketInfo.quat = camera.quaternion;
@@ -200,6 +204,8 @@
 		}
 
 		function initScene() {
+
+        oldLines = new THREE.Object3D();
 
 				eyeBall = new THREE.Mesh(new THREE.SphereGeometry(.03, 10, 10), new THREE.MeshLambertMaterial({
 						color: 0x111111
@@ -304,6 +310,7 @@
 						onMouseUp: function () {
 								draw = false;
 								newLine = true;
+                newCurve.redraw=false;
 								curves.redraw = false;
 
 						},
@@ -311,8 +318,12 @@
 								if (draw) {
 										curves.redraw = true;
 										// console.log(intersection.point);
-										if(intersection)
+										if(intersection){
+                      newCurve.redraw=true;
 											newCurve.push(intersection.point);
+                      if(!newCurve.age)
+                        newCurve.age=0;
+                    }
 								}
 						}
 				});
@@ -551,30 +562,87 @@
 
 		}
 
+		function removeLines(){
+
+			if (lines) {
+					scene.remove(lines);
+					lines.traverse(function (obj) {
+							if (obj instanceof THREE.Mesh) {
+									obj.geometry.dispose();
+									obj.material.dispose();
+							}
+							obj = null;
+					});
+			}
+      // console.log(lines.children.length);
+
+			lines = new THREE.Object3D();
+			scene.add(lines);
+
+      if(curves.length>5){
+        curves.shift();
+      }
+
+      for(var i = 0 ; i < curves.length ; i++){
+        if(!curves[i].redraw)
+          curves[i].age++;
+        else
+          curves[i].age++;
+        if(i<curves.length-1 && curves[i].age>100){
+          curves.splice(i,1);
+        }
+        if(curves[i].length>100){
+          curves[i].shift();
+        }
+      }
+      // console.log('curves');
+
+		}
+
 		function drawLine(arr) {
 
-				if (arr.redraw == true) {
-						if (lines) {
-								scene.remove(lines);
-								lines.traverse(function (obj) {
-										if (obj instanceof THREE.Mesh) {
-												obj.geometry.dispose();
-												obj.material.dispose();
-										}
-										obj = null;
-								});
-						}
+        // removeLines();
+        var geom = new THREE.SphereGeometry( .1,4,4 );
+        var mata = new THREE.MeshLambertMaterial({color:0x555555,side:THREE.DoubleSide});
 
-						lines = new THREE.Object3D();
-						scene.add(lines);
-
+				if (arr.redraw === true) {
+			
 						if (arr.length > 0) {
 								for (var i = 0; i < arr.length; i++) {
-										if (arr[i].length > 1) {
-												var tube = new THREE.Mesh(new THREE.TubeGeometry(new THREE.SplineCurve3(arr[i]), arr[i].length, .02), new THREE.MeshLambertMaterial({
-														color: 0x111111
-												}));
-												lines.add(tube);
+										if (arr[i].length > 3) {
+                      var tube = new THREE.Mesh(new THREE.TubeGeometry(new THREE.SplineCurve3(arr[i]), arr[i].length, Math.max(.0001,.04-(.0001+arr[i].age*.0004))), new THREE.MeshLambertMaterial({
+                            color: 0x111111
+                        }));
+                        lines.add(tube);
+                      for(var j=0 ; j<arr[i].length ; j++){
+                       
+                        var mesh = new THREE.Mesh(geom,mata);
+                        mesh.position.x = arr[i][j].x;
+                        mesh.position.y = arr[i][j].y;
+                        mesh.position.z = arr[i][j].z;
+
+                        if(!arr[i][j].age)
+                          arr[i][j].age=0.001;
+                        else
+                          arr[i][j].age++;
+
+                        mesh.position.x += .1*(Math.sin(Math.PI*2*noise(arr[i][j].x,arr[i][j].y,arr[i][j].z)*arr[i][j].age*.1));
+                        mesh.position.y += .1*(Math.cos(Math.PI*2*noise(arr[i][j].x,arr[i][j].y,arr[i][j].z)*arr[i][j].age*.1));
+                        mesh.position.z += .1*(Math.sin(Math.PI*2*noise(arr[i][j].x,arr[i][j].y,arr[i][j].z)*arr[i][j].age*.1));
+                        
+                        // var up = (j/arr[i].length)*5;
+                        // mesh.scale.multiply(new THREE.Vector3(up,up,up));
+                        mesh.scale.multiplyScalar(Math.max(.001,2-(.0001+arr[i][j].age*.051)));
+
+
+
+                        // console.log(mesh.position);
+
+                        // console.log(lines.children.length);         // 
+                        lines.add(mesh);
+                        // scene.add(mesh);
+												
+                      }
 										}
 								}
 						}
@@ -595,4 +663,47 @@
 
 		init();
 		animate();
+
+
+ var noise = function(ix, iy, iz) {
+
+    var x = ix || 0;
+    var y = iy || 0;
+    var z = iz || 0;
+      var X = Math.floor(x)&255, Y = Math.floor(y)&255, Z = Math.floor(z)&255;
+      x -= Math.floor(x); y -= Math.floor(y); z -= Math.floor(z);
+      var u = fade(x), v = fade(y), w = fade(z);
+      var A = p[X  ]+Y, AA = p[A]+Z, AB = p[A+1]+Z,      // HASH COORDINATES OF
+          B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;      // THE 8 CUBE CORNERS,
+      return lerp(w, lerp(v, lerp(u, grad(p[AA  ], x  , y  , z   ),  // AND ADD
+                                     grad(p[BA  ], x-1, y  , z   )), // BLENDED
+                             lerp(u, grad(p[AB  ], x  , y-1, z   ),  // RESULTS
+                                     grad(p[BB  ], x-1, y-1, z   ))),// FROM  8
+                     lerp(v, lerp(u, grad(p[AA+1], x  , y  , z-1 ),  // CORNERS
+                                     grad(p[BA+1], x-1, y  , z-1 )), // OF CUBE
+                             lerp(u, grad(p[AB+1], x  , y-1, z-1 ),
+                                     grad(p[BB+1], x-1, y-1, z-1 ))));
+   };
+   function fade(t) { return t * t * t * (t * (t * 6 - 15) + 10); };
+   function lerp(t, a, b) { return a + t * (b - a); };
+   function grad(hash, x, y, z) {
+      var h = hash & 15;                      // CONVERT LO 4 BITS OF HASH CODE
+      var u = h<8 ? x : y,                    // INTO 12 GRADIENT DIRECTIONS.
+          v = h<4 ? y : h==12||h==14 ? x : z;
+      return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
+   };
+   var p = [ 151,160,137,91,90,15,
+   131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,23,
+   190, 6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,
+   88,237,149,56,87,174,20,125,136,171,168, 68,175,74,165,71,134,139,48,27,166,
+   77,146,158,231,83,111,229,122,60,211,133,230,220,105,92,41,55,46,245,40,244,
+   102,143,54, 65,25,63,161, 1,216,80,73,209,76,132,187,208, 89,18,169,200,196,
+   135,130,116,188,159,86,164,100,109,198,173,186, 3,64,52,217,226,250,124,123,
+   5,202,38,147,118,126,255,82,85,212,207,206,59,227,47,16,58,17,182,189,28,42,
+   223,183,170,213,119,248,152, 2,44,154,163, 70,221,153,101,155,167, 43,172,9,
+   129,22,39,253, 19,98,108,110,79,113,224,232,178,185, 112,104,218,246,97,228,
+   251,34,242,193,238,210,144,12,191,179,162,241, 81,51,145,235,249,14,239,107,
+   49,192,214, 31,181,199,106,157,184, 84,204,176,115,121,50,45,127, 4,150,254,
+   138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180 ];
+   for (var i=0; i < 256 ; i++) p.push(p[i]);
 }());
